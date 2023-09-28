5f9829190ccc5bcfe126997c3f3886bf88bd172c
zeeshanayaz <zeeshanayaz1@gmail.com>
2023-09-28 10:39:11 +0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by muhammad.zeeshan on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
